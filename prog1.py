f9a85c76f76b5975e35c347e4dfe903eb83d8bd9
yadavmk0001-sys <yadavmk0001@gmail.com>
2025-09-02 16:49:42 +0530

    Add files via upload

---
+++ prog1.py
@@ -0,0 +1,10 @@
+string  = input("Please enter your own word")
+char = input("Please enter your own Character: ")
+i = 0
+count = 0
+
+while(i < len(string)):
+    if(string[i]==char):
+        count=count+1
+    i = i+1
+ 
---
+++ prog2.py
@@ -0,0 +1,6 @@
+num = 5
+fact = 1
+
+for i in range(1, num + 1):
+    fact *= i
+print("Factorial of", num, "is", fact)
